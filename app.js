a529f783a6467f0d86a43e68899b9604c70282e9
Tracey <ttrepper@yahoo.com>
2021-06-06 18:38:23 -0400

    add giphy party exercise

---
+++ app.js
@@ -0,0 +1,35 @@
+const $imagearea = $("#image-area");
+const $searchInput = $("#search");
+
+function getGiphy(res) {
+  const ul = document.querySelector("#images");
+  const li = document.createElement("li");
+  let numResults = res;
+  if (numResults) {
+    let randomIdx = Math.floor(Math.random() * numResults);
+    let $addGif = $("<img>", { src: res[randomIdx].images.original.url });
+    li.append($addGif);
+    ul.append(li);
+    console.log("an image is coming");
+  }
+  console.log(numResults);
+}
+
+$("searchform").on("submit", async function (evt) {
+  evt.preventDefault();
+
+  let searchGiphy = $searchInput.val();
+  $searchInput.val("");
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      api_key: "pXRNb3pmfel4eUDipzy3bkTgAzR6Cf9k",
+      q: searchGiphy,
+    },
+  });
+  getGiphy(response.data);
+});
+$("#delete").on("click", function () {
+  $imagearea.empty();
+});
+
+console.log("Let's get this party started!");
