a40afe6a4df726b9a836310cf4e524771da5891a
tbtreppe <63826526+tbtreppe@users.noreply.github.com>
2021-06-08 08:42:01 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
-const $gifArea = $("#gif-area");
+const $imagearea = $("#image-area");
 const $searchInput = $("#search");
 
-/* use ajax result to add a gif */
-
-function addGif(res) {
-  let numResults = res.data.length;
+function getGiphy(res) {
+  const $ul = $("#images");
+  const $li = $("li");
+  let numResults = res;
   if (numResults) {
     let randomIdx = Math.floor(Math.random() * numResults);
-    let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
-    let $newGif = $("<img>", {
-      src: res.data[randomIdx].images.original.url,
-      class: "w-100"
-    });
-    $newCol.append($newGif);
-    $gifArea.append($newCol);
+    let $addGif = $("<img>", { src: res[randomIdx] });
+
+    $li.append($addGif);
+    $ul.append($li);
+    console.log("an image is coming");
   }
+  console.log(numResults);
 }
 
-/* handle form submission: clear search box & make ajax call */
-
-$("form").on("submit", async function(evt) {
+$("#searchform").on("submit", async function (evt) {
   evt.preventDefault();
 
-  let searchTerm = $searchInput.val();
+  let searchGiphy = $searchInput.val();
   $searchInput.val("");
-
   const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
     params: {
-      q: searchTerm,
-      api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
-    }
+      api_key: "pXRNb3pmfel4eUDipzy3bkTgAzR6Cf9k",
+      q: searchGiphy,
+    },
   });
-  addGif(response.data);
+  getGiphy(response.data);
+});
+$("#delete").on("click", function () {
+  $imagearea.empty();
 });
 
-/* remove gif */
-
-$("#remove").on("click", function() {
-  $gifArea.empty();
-});
+console.log("Let's get this party started!");
